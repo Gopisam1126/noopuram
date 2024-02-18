ad828d38ab392d7655e88d243944d21cfdba1ce3
Gopisam1126 <gopisambhu1126@gmail.com>
2024-02-18 21:50:51 +0530

    netlify files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
+import ServerlessHttp from "serverless-http";
+import { Router } from "express";
 
 const app = express();
 const port = 3000;
