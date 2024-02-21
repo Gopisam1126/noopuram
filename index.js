3ed4845d4139fbf61170791dfa4474e7292630f7
Gopisam1126 <gopisambhu1126@gmail.com>
2024-02-21 10:19:26 +0530

    resolve aws error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
-import ServerlessHttp from "serverless-http";
-import { Router } from "express";
+// import ServerlessHttp from "serverless-http";
+// import { Router } from "express";
 
 const app = express();
 const router = express.Router
@@ -95,5 +95,5 @@ app.listen(port, () => {
     console.log(`server running on port ${port}`);
 });
 
-app.use('/.netlify/functions/api', router);
-// module.exports.handler = Serverless(app);
+// app.use('/.netlify/functions/api', router);
+// // module.exports.handler = Serverless(app);
